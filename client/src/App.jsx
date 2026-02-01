f4e72dc49faa718dff98dc1ad3e051986040d875
instagramredirectip <karthikanilcolab@gmail.com>
2026-02-01 17:46:15 +0000

    admin update fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,48 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { AuthProvider } from './context/AuthContext';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
-import Payouts from './pages/Payouts'; // <--- Import Payouts
+import Payouts from './pages/Payouts';
+import AdminPanel from './pages/AdminPanel'; // <--- THIS WAS MISSING
 import ProtectedRoute from './components/ProtectedRoute';
-import { Toaster } from 'react-hot-toast';
-import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
     <AuthProvider>
-      <Toaster position="top-center" />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        
-        {/* Protected Routes */}
-        <Route path="/dashboard" element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        } />
-        
-        {/* New Payouts Route */}
-        <Route path="/payouts" element={
-          <ProtectedRoute>
-            <Payouts />
-          </ProtectedRoute>
-        } />
+      <Router>
+        <Toaster position="top-right" />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          
+          {/* Protected User Routes */}
+          <Route path="/dashboard" element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          } />
+          
+          <Route path="/payouts" element={
+            <ProtectedRoute>
+              <Payouts />
+            </ProtectedRoute>
+          } />
 
-        <Route path="/admin" element={<AdminPanel />} />
+          {/* Admin Route */}
+          <Route path="/admin" element={
+            <ProtectedRoute>
+              <AdminPanel />
+            </ProtectedRoute>
+          } />
 
-      </Routes>
+          {/* Catch all - Redirect to Home */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
     </AuthProvider>
   );
 }
